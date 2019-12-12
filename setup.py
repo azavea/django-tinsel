df897a3692f1b6b980eb03ca0aba7a48f9a83244
Justin Walgran <jwalgran@azavea.com>
2019-12-12 11:53:40 -0700

    Declare to PyPI and tools that django-tinsel supports Python 3
    We add the "trove classifiers" matching the versions listed in the TravisCI
    build matrix so that tools like `caniusepython3` will list the library as
    supported.
    
    https://github.com/brettcannon/caniusepython3#how-do-you-tell-if-a-project-has-been-ported-to-python-3

--- setup.py
+++ setup.py
@@ -20,6 +20,9 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         "Environment :: Plugins",
         "Framework :: Django",
         "License :: OSI Approved :: Apache Software License"
