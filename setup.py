c5ba372b80014a04a270568595864f7664219981
Justin Walgran <jwalgran@azavea.com>
2019-12-16 15:43:59 -0700

    Add Python 3.7 and 3.8 to the Travis build matrix
    These are the two latest versions of Python.

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "Environment :: Plugins",
         "Framework :: Django",
         "License :: OSI Approved :: Apache Software License"
